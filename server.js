98a8aa1a32e00080d421ca6c32ec1fcb55b6cd66
Abdullah <dev.waqas08@gmail.com>
2017-11-15 12:37:23 +0500

    changes

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
+// const express = require('express');
+// const http = require('http');
+// const path = require('path');
+
+// const app = express();
+// // Run the app by serving the static files
+// // in the dist directory
+// app.use(express.static(__dirname + 'dist'));
+
+// app.listen(process.env.PORT || 8080);
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+// console.log("Console Listening!");
+
+
 const express = require('express');
-const http = require('http');
-const path = require('path');
 
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
-console.log("Console Listening!");
+// console.log("Console Listening!");
